c185b143255e3fd70cc2a10de7ab46e0db3c7dab
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-14 02:18:57 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,98 +1,36 @@
 # frozen_string_literal: true
 
 # name: discourse-lottery-v9
-# about: Adds the ability to create lottery activities in topics
-# version: 1.0
-# author: Based on discourse-calendar
-# url: https://github.com/discourse/discourse-lottery
+# about: A Discourse plugin to create lotteries in posts.
+# version: 0.1
+# authors: Your Name
+# url: https://github.com/your-repo/discourse-lottery-v9
 
+# 1. 注册站点设置
 enabled_site_setting :lottery_enabled
 
-# 注册前端资源
+# 2. 注册前端资源 (CSS, SVG, 第三方 JS 模块)
 register_asset "stylesheets/common/discourse-post-lottery.scss"
-register_asset "stylesheets/common/discourse-post-lottery-preview.scss"
-register_asset "stylesheets/mobile/discourse-post-lottery.scss", :mobile
-register_asset "stylesheets/colors.scss", :color_definitions
+# 如果你已经创建了这些文件，可以取消注释，否则请保持注释状态以避免“找不到文件”的错误
+# register_asset "stylesheets/common/discourse-post-lottery-preview.scss"
+# register_asset "stylesheets/mobile/discourse-post-lottery.scss", :mobile
+# register_asset "stylesheets/colors.scss", :color_definitions
 
 register_svg_icon "gift"
 register_svg_icon "trophy"
 register_svg_icon "dice"
 register_svg_icon "users"
 
-# 【关键修复】将这一行从 after_initialize 移到了这里
+# 关键：`register_module` 必须在顶层作用域，不能在 `after_initialize` 内部
 register_module("ember-math-helpers", "ember-math-helpers/dist/modules/helpers.js", type: :es6)
 
+# 3. 定义插件的 Ruby 模块 (可选，但推荐)
 module ::DiscoursePostLottery
-  PLUGIN_NAME = "discourse-post-lottery"
-  
-  TOPIC_POST_LOTTERY_STARTS_AT = "TopicLotteryStartsAt"  
-  TOPIC_POST_LOTTERY_ENDS_AT = "TopicLotteryEndsAt"
+  PLUGIN_NAME = "discourse-lottery-v9"
 end
 
-# 你的代码中没有这一行，这是正确的，因为我们还没创建 engine.rb
-# require_relative "lib/discourse_post_lottery/engine"
-
+# 4. 在 Discourse 初始化后运行的代码块 (目前为空)
 after_initialize do
-  # 你的代码中没有这一行，这是正确的，因为我们还没创建 post_extension.rb
-  # require_relative "lib/discourse_post_lottery/post_extension"
-
-  # reloadable_patch do
-  #   Post.prepend(DiscoursePostLottery::PostExtension)
-  # end
-
-  # 用户权限 (保持原样)
-  add_to_class(:user, :can_create_discourse_post_lottery?) do
-    return @can_create_discourse_post_lottery if defined?(@can_create_discourse_post_lottery)
-    @can_create_discourse_post_lottery =
-      begin
-        return true if staff?
-        allowed_groups = SiteSetting.lottery_allowed_on_groups.to_s.split("|").compact
-        allowed_groups.present? &&
-          (
-            allowed_groups.include?(Group::AUTO_GROUPS[:everyone].to_s) ||
-              groups.where(id: allowed_groups).exists?
-          )
-      rescue StandardError
-        false
-      end
-  end
-
-  add_to_class(:guardian, :can_create_discourse_post_lottery?) do
-    user && user.can_create_discourse_post_lottery?
-  end
-
-  add_to_class(:guardian, :can_act_on_discourse_post_lottery?) do |lottery|
-    user && user.staff? || (user && Guardian.new(user).can_edit_post?(lottery.post))
-  end
-
-  add_to_serializer(:current_user, :can_create_discourse_post_lottery) do
-    object.can_create_discourse_post_lottery?
-  end
-
-  # 前端需要的序列化 (保持原样)
-  add_to_serializer(
-    :post,
-    :lottery,
-    include_condition: -> do
-      SiteSetting.lottery_enabled && !object.nil? && !object.deleted_at.present?
-    end,
-  ) { object.lottery ? DiscoursePostLottery::LotterySerializer.new(object.lottery, scope: scope, root: false) : nil }
-
-  # Topic自定义字段 (保持原样)
-  add_preloaded_topic_list_custom_field DiscoursePostLottery::TOPIC_POST_LOTTERY_STARTS_AT
-
-  add_to_class(:topic, :lottery_starts_at) do
-    @lottery_starts_at ||= custom_fields[DiscoursePostLottery::TOPIC_POST_LOTTERY_STARTS_AT]
-  end
-
-  add_to_serializer(
-    :topic_list_item,
-    :lottery_starts_at,
-    include_condition: -> do
-      SiteSetting.lottery_enabled && object.lottery_starts_at
-    end,
-  ) { object.lottery_starts_at }
-
-  # 注册自定义字段 (保持原样)
-  register_post_custom_field_type("discourse_post_lottery", :string)
+  # 我们所有的后端逻辑（如 Guardian 补丁, Post 扩展, 序列化器）都将在这里添加。
+  # 目前，为了确保前端能够构建成功，我们暂时将此块留空。
 end
