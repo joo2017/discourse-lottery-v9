509e7bd2913d07304ab81a3f257e8f43f4c6bdc5
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-14 02:04:22 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,107 +1,39 @@
 # frozen_string_literal: true
 
 # name: discourse-lottery-v9
-# about: A Discourse plugin to create lottery activities in topics
-# version: 1.0
-# author: Based on discourse-calendar
-# url: https://github.com/discourse/discourse-lottery
+# about: A Discourse plugin to create lotteries in posts.
+# version: 0.1
+# authors: Your Name
+# url: https://github.com/your-repo/discourse-lottery-v9
 
 enabled_site_setting :lottery_enabled
 
 # 注册前端资源
 register_asset "stylesheets/common/discourse-post-lottery.scss"
-register_asset "stylesheets/common/discourse-post-lottery-preview.scss"
-register_asset "stylesheets/mobile/discourse-post-lottery.scss", :mobile
-register_asset "stylesheets/colors.scss", :color_definitions
+# 下面这两行是我们还没有创建的文件，为了让构建通过，暂时注释掉
+# register_asset "stylesheets/common/discourse-post-lottery-preview.scss"
+# register_asset "stylesheets/mobile/discourse-post-lottery.scss", :mobile
+# register_asset "stylesheets/colors.scss", :color_definitions
 
 register_svg_icon "gift"
 register_svg_icon "trophy"
 register_svg_icon "dice"
 register_svg_icon "users"
 
-# 关键改动：将 register_module 从 after_initialize 块中移到这里
-# Discourse 核心不包含 ember-math-helpers，所以我们需要为插件注册这个模块。
-# 这会让它在前端的 `import` 语句中可用。
+# 注册 ember-math-helpers 模块，供前端使用
 register_module("ember-math-helpers", "ember-math-helpers/dist/modules/helpers.js", type: :es6)
 
+# 插件的主模块定义
 module ::DiscoursePostLottery
   PLUGIN_NAME = "discourse-post-lottery"
   
+  # 这些常量目前在后端还未使用，但预留是好的
   TOPIC_POST_LOTTERY_STARTS_AT = "TopicLotteryStartsAt"  
   TOPIC_POST_LOTTERY_ENDS_AT = "TopicLotteryEndsAt"
 end
 
-# 这行代码应该是存在的，但你的原始代码中没有，我先注释掉
-# 如果你已经创建了这个文件，请取消注释
-# require_relative "lib/discourse_post_lottery/engine"
-
+# after_initialize 块用于在 Discourse 核心加载完毕后执行代码
 after_initialize do
-  # 加载模型扩展
-  # 下面的路径可能需要根据你的实际目录结构调整
-  # require_relative "lib/discourse_post_lottery/post_extension"
-
-  # 前端需要的扩展
-  # reloadable_patch do
-  #   Post.prepend(DiscoursePostLottery::PostExtension)
-  # end
-
-  # 用户权限
-  add_to_class(:user, :can_create_discourse_post_lottery?) do
-    return @can_create_discourse_post_lottery if defined?(@can_create_discourse_post_lottery)
-    @can_create_discourse_post_lottery =
-      begin
-        return true if staff?
-        # 注意: 这里的设置名应该是 lottery_allowed_on_groups
-        # 你之前的代码中写的是 lottery_allowed_on_groups
-        allowed_groups = SiteSetting.lottery_allowed_on_groups.to_s.split("|").compact
-        allowed_groups.present? &&
-          (
-            allowed_groups.include?(Group::AUTO_GROUPS[:everyone].to_s) ||
-              groups.where(id: allowed_groups).exists?
-          )
-      rescue StandardError
-        false
-      end
-  end
-
-  add_to_class(:guardian, :can_create_discourse_post_lottery?) do
-    user && user.can_create_discourse_post_lottery?
-  end
-
-  add_to_class(:guardian, :can_act_on_discourse_post_lottery?) do |lottery|
-    # 确保 lottery 对象存在并且有关联的 post
-    user && lottery&.post && (user.staff? || Guardian.new(user).can_edit_post?(lottery.post))
-  end
-
-  add_to_serializer(:current_user, :can_create_discourse_post_lottery) do
-    object.can_create_discourse_post_lottery?
-  end
-
-  # 前端需要的序列化
-  add_to_serializer(
-    :post,
-    :lottery,
-    include_condition: -> do
-      # 检查 object.lottery 是否存在
-      SiteSetting.lottery_enabled && object&.lottery && !object.deleted_at.present?
-    end,
-  ) { object.lottery ? DiscoursePostLottery::LotterySerializer.new(object.lottery, scope: scope, root: false) : nil }
-
-  # Topic自定义字段（前端显示需要）
-  add_preloaded_topic_list_custom_field DiscoursePostLottery::TOPIC_POST_LOTTERY_STARTS_AT
-
-  add_to_class(:topic, :lottery_starts_at) do
-    @lottery_starts_at ||= custom_fields[DiscoursePostLottery::TOPIC_POST_LOTTERY_STARTS_AT]
-  end
-
-  add_to_serializer(
-    :topic_list_item,
-    :lottery_starts_at,
-    include_condition: -> do
-      SiteSetting.lottery_enabled && object.lottery_starts_at
-    end,
-  ) { object.lottery_starts_at }
-
-  # 注册自定义字段
-  register_post_custom_field_type("discourse_post_lottery", :string)
+  # 目前我们还没有任何后端逻辑，所以这个块是空的。
+  # 未来的后端代码，如 post_extension, guardian 扩展等，都将放在这里。
 end
